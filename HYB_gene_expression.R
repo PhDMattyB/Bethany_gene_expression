f28e65f0efcd13235931ff2c1d9335caa216a14b
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2025-02-04 19:01:34 +0000

    variant filter
    Filter out all invariant sites

--- HYB_gene_expression.R
+++ HYB_gene_expression.R
@@ -24,3 +24,97 @@ names(brain_data)
 liver_meta = read_csv('hyb_data.csv')
 liver_data = read_tsv('liver_gene_read_counts_table_all_final.tsv')
 
+
+# Brain variant filter ----------------------------------------------------
+
+brain_data_rot = brain_data %>%
+  remove_rownames() %>%
+  column_to_rownames(var = 'GeneID') %>%
+  t() %>%
+  as_tibble()
+
+brain_full = bind_cols(brain_meta, 
+                      brain_data_rot)
+
+brain_var = brain_full %>%
+  select(pop, 
+         type,
+         temp,
+         starts_with('ENSGAC')) %>% 
+  group_by(pop, 
+           type, 
+           temp) %>%
+  summarise(across(everything(), var, na.rm = F))%>%
+  ungroup() %>% 
+  select(starts_with('ENSGAC')) %>% 
+  colSums(abs(.)>0) %>% 
+  as.data.frame() %>%
+  rownames_to_column() %>% 
+  as_tibble() %>% 
+  rename(variance = 2) %>% 
+  filter(variance > 0.000000e+00)
+
+brain_orig_data = brain_data %>%
+  rownames_to_column() %>% 
+  as_tibble()
+
+brain_cleaned = left_join(brain_var, 
+                        brain_orig_data, 
+                        by = 'rowname') %>% 
+  select(-variance)
+
+brain_cleaned = brain_cleaned %>% 
+  as.data.frame() %>% 
+  remove_rownames() %>% 
+  column_to_rownames(var = 'rowname')
+
+
+# Brain GLMER -------------------------------------------------------------
+
+
+# liver variant filter ----------------------------------------------------
+
+liver_data_rot = liver_data %>%
+  remove_rownames() %>%
+  column_to_rownames(var = 'GeneID') %>%
+  t() %>%
+  as_tibble()
+
+liver_full = bind_cols(liver_meta, 
+                       liver_data_rot)
+
+liver_var = liver_full %>%
+  select(pop, 
+         type,
+         temp,
+         starts_with('ENSGAC')) %>% 
+  group_by(pop, 
+           type, 
+           temp) %>%
+  summarise(across(everything(), var, na.rm = F))%>%
+  ungroup() %>% 
+  select(starts_with('ENSGAC')) %>% 
+  colSums(abs(.)>0) %>% 
+  as.data.frame() %>%
+  rownames_to_column() %>% 
+  as_tibble() %>% 
+  rename(variance = 2) %>% 
+  filter(variance > 0.000000e+00)
+
+liver_orig_data = liver_data %>%
+  rownames_to_column() %>% 
+  as_tibble()
+
+liver_cleaned = left_join(liver_var, 
+                          liver_orig_data, 
+                          by = 'rowname') %>% 
+  select(-variance)
+
+liver_cleaned = liver_cleaned %>% 
+  as.data.frame() %>% 
+  remove_rownames() %>% 
+  column_to_rownames(var = 'rowname')
+
+# liver GLMER -------------------------------------------------------------
+
+
