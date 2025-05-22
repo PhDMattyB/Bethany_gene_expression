a659487e8a1955723c5bd528a9639395f5cb0f03
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2025-05-22 13:57:35 +0100

    Create SKR_hyrbid_analysis.R

---
+++ SKR_hyrbid_analysis.R
@@ -0,0 +1,15 @@
+##############################
+## Bethany Gene expression
+## SKR HYBRIDS
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 22.05.2025
+##
+##############################
+
+
+setwd('~/Parsons_Postdoc/SKR_Hybrid_Gene_expression/')
+
+library(tidyverse)
+
