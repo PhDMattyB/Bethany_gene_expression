03afb88fcecdcb4bdd91ba64e6b922de4e910b3e
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2025-07-15 13:49:34 +0100

    Create SKR_transgressive_ASEP.R

---
+++ SKR_transgressive_ASEP.R
@@ -0,0 +1,15 @@
+##############################
+## Transgressive allele specific expression 
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 15.07.2024
+##
+##############################
+
+
+setwd('~/Parsons_Postdoc/SKR_Hybrid_Gene_expression/')
+
+library(tidyverse)
+
+ecotype_snps = read_csv('ecotype_unique_snps_all.csv')
